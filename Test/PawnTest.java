372dbff08ade89e466af92d68b85630052426281
woonohyo <wonhyo.yi@gmail.com>
2013-07-12 15:49:37 +0900

    Exercise 1-4

--- Test/PawnTest.java
+++ Test/PawnTest.java
@@ -7,7 +7,18 @@ import junit.framework.TestCase;
 
 public class PawnTest extends TestCase {
 	public void testCreate() throws Exception {
-		Pawn pawn1 = new Pawn();
+		String white = "white";
+		String black = "black";
+
+		Pawn pawn1 = new Pawn(white);
+		String color1 = pawn1.getColor();
+		assertEquals("white", color1);
+		
+		Pawn pawn2 = new Pawn(black);
+		String color2 = pawn2.getColor();
+		assertEquals("black", color2);
+		
+		
 		
 	}
 
