e7b169a6a19d657dcbb8745d0629b1c19df5f36b
woonohyo <wonhyo.yi@gmail.com>
2013-07-12 15:30:08 +0900

    Exericse 1-2 Test Class made

--- Test/PawnTest.java
+++ Test/PawnTest.java
@@ -1,6 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 
 public class PawnTest extends TestCase {
+	public void testCreate() throws Exception {
+		
+	}
 
 }
