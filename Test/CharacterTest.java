a6acd2f8db9d69fbf7fd893ba7f3d5cd858c3e82
woonohyo <wonhyo.yi@gmail.com>
2013-07-20 11:47:56 +0900

    Exercise 3-1

--- Test/CharacterTest.java
+++ Test/CharacterTest.java
@@ -3,13 +3,9 @@ import junit.framework.TestCase;
 
 public class CharacterTest extends TestCase {
 	public void testWhitespace() throws Exception {
-//		char NEWLINE = 
 		assertEquals(true, Character.isWhitespace('\n'));
 		assertEquals(true, Character.isWhitespace('\t'));
-		assertEquals(true, Character.isWhitespace(' '));
-		
-		
-		
+		assertEquals(true, Character.isWhitespace(' '));		
 	}
 
 }
