e5c48d6ae8314cd70d7df35919f5ecce0bad2185
woonohyo <wonhyo.yi@gmail.com>
2013-07-20 11:58:09 +0900

    Exercise 3-2

--- Test/CharacterTest.java
+++ Test/CharacterTest.java
@@ -5,7 +5,13 @@ public class CharacterTest extends TestCase {
 	public void testWhitespace() throws Exception {
 		assertEquals(true, Character.isWhitespace('\n'));
 		assertEquals(true, Character.isWhitespace('\t'));
-		assertEquals(true, Character.isWhitespace(' '));		
+		assertEquals(true, Character.isWhitespace(' '));
+		
+		assertEquals(false, Character.isUnicodeIdentifierStart('!'|'@'|'#'|'$'|'%'|'^'|'&'|'*'|'('|')'|'_'|'-'));
+		assertEquals(false, Character.isUnicodeIdentifierStart('1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'|'0'));
+		
+		assertEquals(true, Character.isUnicodeIdentifierStart('민'|'주'));
+		
 	}
 
 }
